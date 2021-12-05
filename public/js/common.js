de05c19f940cde4c13cd45a283e4bf0fba9974b1
Pavel <wol1414@gmail.com>
2021-12-05 16:17:24 +0300

    start

---
+++ public/js/common.js
@@ -0,0 +1,359 @@
+
+let div = document.createElement('div');
+
+div.style.overflowY = 'scroll';
+div.style.width = '50px';
+div.style.height = '50px';
+document.body.append(div);
+
+let scrollWidth = div.offsetWidth - div.clientWidth;
+
+div.remove();
+
+
+const JSCCommon = {
+	
+	btnToggleMenuMobile: [].slice.call(document.querySelectorAll(".toggle-menu-mobile--js")),
+	menuMobile: document.querySelector(".menu-mobile--js"),
+	menuMobileLink: [].slice.call(document.querySelectorAll(".menu-mobile--js ul li a")),
+	
+	modalCall() {
+		
+		$("[data-fancybox]").fancybox({
+			beforeLoad: function () {
+				if (!document.querySelector("html").classList.contains(".fixed")) document.querySelector("html").style.marginRight = scrollWidth + 'px';
+			},
+			afterClose: function () {
+				if (!document.querySelector("html").classList.contains(".fixed")) document.querySelector("html").style.marginRight = null;
+				// 	document.querySelector("html").classList.remove("fixed")
+			},
+		})
+		$(".link-modal-js").fancybox({
+			arrows: false,
+			infobar: false,
+			touch: false,
+			type: 'inline',
+			autoFocus: false,
+			i18n: {
+				en: {
+					CLOSE: "Закрыть",
+					NEXT: "Вперед",
+					PREV: "Назад",
+					// PLAY_START: "Start slideshow",
+					// PLAY_STOP: "Pause slideshow",
+					// FULL_SCREEN: "Full screen",
+					// THUMBS: "Thumbnails",
+					// DOWNLOAD: "Download",
+					// SHARE: "Share",
+					// ZOOM: "Zoom"
+				},
+			},
+			beforeLoad: function () {
+				if (!document.querySelector("html").classList.contains(".fixed")) document.querySelector("html").style.marginRight = scrollWidth + 'px';
+			},
+			afterClose: function () {
+				if (!document.querySelector("html").classList.contains(".fixed")) document.querySelector("html").style.marginRight = null;
+			// 	document.querySelector("html").classList.remove("fixed")
+			},
+		});
+
+		$.fancybox.defaults.backFocus = false;
+		const linkModal = document.querySelectorAll('.link-modal-js');
+		function addData() {
+			linkModal.forEach(element => {
+				element.addEventListener('click', () => {
+					let modal = document.querySelector(element.getAttribute("href"));
+					const data = element.dataset;
+
+					function setValue(val, elem) {
+						if (elem && val) {
+							const el = modal.querySelector(elem)
+							el.tagName == "INPUT"
+								? el.value = val
+								: el.innerHTML = val;
+							// console.log(modal.querySelector(elem).tagName)
+						}
+					}
+					setValue(data.title, '.ttu');
+					setValue(data.text, '.after-headline');
+					setValue(data.btn, '.btn');
+					setValue(data.order, '.order');
+				})
+			})
+		}
+		if (linkModal) addData();
+	},
+	// /modalCall
+	toggleMenu() {
+		const toggle = this.btnToggleMenuMobile;
+		const menu = this.menuMobile;
+		document.addEventListener("click", function (event) {
+			const toggleEv = event.target.closest(".toggle-menu-mobile--js");
+			if (!toggleEv) return;
+			toggle.forEach(el => el.classList.toggle("on"));
+			menu.classList.toggle("active");
+			[document.body, document.querySelector('html')].forEach(el => el.classList.toggle("fixed"));
+
+		}, { passive: true });
+	},
+	closeMenu() {
+		if (!this.menuMobile) return;
+		this.btnToggleMenuMobile.forEach(element => element.classList.remove("on"));
+		this.menuMobile.classList.remove("active");
+		[document.body, document.querySelector('html')].forEach(el => el.classList.remove("fixed"));
+
+	},
+	mobileMenu() {
+		if (!this.menuMobileLink) return;
+		this.toggleMenu();
+		document.addEventListener('mouseup', (event) => {
+			let container = event.target.closest(".menu-mobile--js.active"); // (1)
+			let link = event.target.closest(".navMenu__link"); // (1)
+			if (!container || link) this.closeMenu();
+		}, { passive: true });
+
+		window.addEventListener('resize', () => {
+			if (window.matchMedia("(min-width: 992px)").matches) this.closeMenu();
+		}, { passive: true });
+	},
+	// /mobileMenu
+
+	// tabs  .
+	tabscostume(tab) {
+		const tabs = document.querySelectorAll(tab);
+		// const indexOf = element => Array.from(element.parentNode.children).indexOf(element);
+		tabs.forEach(element => {
+			let tabs = element;
+			const tabsCaption = tabs.querySelector(".tabs__caption");
+			const tabsBtn = tabsCaption.querySelectorAll(".tabs__btn");
+			const tabsWrap = tabs.querySelector(".tabs__wrap");
+			const tabsContent = tabsWrap.querySelectorAll(".tabs__content");
+			const random = Math.trunc(Math.random() * 1000);
+			tabsBtn.forEach((el, index) => {
+				const data = `tab-content-${random}-${index}`;
+				el.dataset.tabBtn = data;
+				const content = tabsContent[index];
+				content.dataset.tabContent = data;
+				if (!content.dataset.tabContent == data) return;
+
+				// const active = content.classList.contains('active') ? 'active' : '';
+				// console.log(el.innerHTML);
+				// content.insertAdjacentHTML("beforebegin", `<div class="tabs__btn-accordion  btn btn-primary d-block mb-1 ${active}" data-tab-btn="${data}">${el.innerHTML}</div>`)
+			})
+
+
+			tabs.addEventListener('click', function (element) {
+				const btn = element.target.closest(`[data-tab-btn]:not(.active)`);
+				if (!btn) return;
+				const data = btn.dataset.tabBtn;
+				const tabsAllBtn = this.querySelectorAll(`[data-tab-btn`);
+				const content = this.querySelectorAll(`[data-tab-content]`);
+				tabsAllBtn.forEach(element => {
+					element.dataset.tabBtn == data
+						? element.classList.add('active')
+						: element.classList.remove('active')
+				});
+				content.forEach(element => {
+					element.dataset.tabContent == data
+						? (element.classList.add('active'))
+						: element.classList.remove('active')
+				});
+			})
+		})
+
+		// $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
+		// 	$(this)
+		// 		.addClass('active').siblings().removeClass('active')
+		// 		.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
+		// 		.eq($(this).index()).fadeIn().addClass('active');
+
+		// });
+
+	},
+	// /tabs
+
+	inputMask() {
+		// mask for input
+		let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
+		InputTel.forEach(element => element.setAttribute("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"));
+		Inputmask("+9(999)999-99-99").mask(InputTel);
+	},
+	// /inputMask
+	ifie() {
+		var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+		if (isIE11) {
+			document.body.insertAdjacentHTML("beforeend", '<div class="browsehappy">	<p class=" container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p></div>');
+		}
+	},
+
+	heightwindow() {
+		// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+		let vh = window.innerHeight * 0.01;
+		// Then we set the value in the --vh custom property to the root of the document
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+		// We listen to the resize event
+		window.addEventListener('resize', () => {
+			// We execute the same script as before
+			let vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		}, { passive: true });
+	},
+	animateScroll() {
+
+		$(document).on('click', " .top-nav li a, .scroll-link", function () {
+			const elementClick = $(this).attr("href");
+			const destination = $(elementClick).offset().top;
+
+			$('html, body').animate({ scrollTop: destination }, 1100);
+
+			return false;
+		});
+	},
+
+};
+const $ = jQuery;
+
+function eventHandler() {
+	JSCCommon.ifie();
+	JSCCommon.modalCall();
+	JSCCommon.tabscostume('.tabs--js');
+	JSCCommon.mobileMenu();
+	JSCCommon.inputMask();
+	JSCCommon.heightwindow();
+	JSCCommon.animateScroll();
+
+	var x = window.location.host;
+	let screenName;
+	screenName = 'about-4.jpg';
+	if (screenName && x.includes("localhost:30")) {
+		document.body.insertAdjacentHTML("beforeend", `<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
+	}
+
+	function whenResize() {
+		let topNav = document.querySelector('.top-nav  ');
+		if (!topNav) return;
+		window.addEventListener('scroll', function (e) {
+			this.scrollY > 100
+				? topNav.classList.add('fixed')
+				: topNav.classList.remove('fixed');
+		}, { passive: true })
+
+	}
+
+	window.addEventListener('resize', () => {
+		whenResize();
+
+	}, { passive: true });
+
+	whenResize();
+
+
+
+	const promotionSlider = new Swiper('.sPromotions__slider--js', {
+		slidesPerView: 'auto',
+		// loop: true,
+		watchOverflow: true,
+		spaceBetween: 18,
+		// autoHeight: true,
+		breakpoints: {
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 30
+			}
+		}
+	});
+
+	const sertificatesSlider = new Swiper('.sCertificates__slider--js', {
+		slidesPerView: 'auto',
+		watchOverflow: true,
+		spaceBetween: 16,
+		// autoHeight: true,
+		breakpoints: {
+			768: {
+				slidesPerView: 3,
+				direction: 'vertical',
+				spaceBetween: 0
+			}
+		}
+	});
+
+	const mastersSlider = new Swiper('.sMasters__slider--js', {
+		slidesPerView: 'auto',
+		watchOverflow: true,
+		spaceBetween: 16,
+		breakpoints: {
+			992: {
+				slidesPerView: 4,
+				slidesPerColumn: 2,
+				spaceBetween: 30,
+			}
+		}
+	});
+
+
+	const haircutsSlider = new Swiper('.sHaircuts__slider--js', {
+		slidesPerView: 'auto',
+		// loop: true,
+		spaceBetween: 18,
+		// freeMode: true,
+			// freeModeMomentum: true,
+			// spaceBetween: 30,
+			watchOverflow: true,
+		breakpoints: {
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 30
+			}
+		}
+	});
+
+	const tabSlider = new Swiper('.sPrice__slider--js', {
+		slidesPerView: 'auto',
+		spaceBetween: 8,
+		watchOverflow: true,
+		breakpoints: {
+			768: {
+				spaceBetween: 16
+			}
+		}
+	});
+
+	$('.drop-accardion-js').on('click', function(){
+		$(this).toggleClass('active').parent().toggleClass('active').find('.drop-accardion-toggle-js').toggleClass('active');
+	})
+
+	function makeDDGroup(qSelecorts){
+    for (let parentSelect of qSelecorts){
+      let parent = document.querySelector(parentSelect);
+      if (parent){
+        // childHeads, kind of funny))
+        let ChildHeads = parent.querySelectorAll('.accardion__head--js');
+        $(ChildHeads).click(function (){
+          let clickedHead = this;
+          $(ChildHeads).each(function (){
+            if (this === clickedHead){
+              $(this.parentElement).toggleClass('active');
+              $(this.parentElement).find('.accardion__content--js').slideToggle(function (){
+                $(this).toggleClass('active');
+              });
+            }
+            else{
+              $(this.parentElement).removeClass('active');
+              $(this.parentElement).find('.accardion__content--js').slideUp(function (){
+                $(this).removeClass('active');
+              });
+            }
+          });
+        });
+      }
+    }
+  }
+  makeDDGroup(['.sVacancies', '.dd-price-js']);
+
+};
+if (document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+1. <https://aleksandr3012.github.io/roots/public/>
+1. <https://aleksandr3012.github.io/roots/public/02-masters.html>
+1. <https://aleksandr3012.github.io/roots/public/03-master-card.html>
+1. <https://aleksandr3012.github.io/roots/public/04_1-sbout.html>
+1. <https://aleksandr3012.github.io/roots/public/04_2-sbout.html>
+1. <https://aleksandr3012.github.io/roots/public/04_3-sbout.html>
+1. <https://aleksandr3012.github.io/roots/public/04_4-sbout.html>
+1. <https://aleksandr3012.github.io/roots/public/04_5-sbout.html>
